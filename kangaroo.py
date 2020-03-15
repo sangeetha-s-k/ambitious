a8905a2751586a16e329372e652037deed402d87
sangeetha-s-k <62176851+sangeetha-s-k@users.noreply.github.com>
2020-03-15 16:48:30 +0530

    Update kangaroo.py

--- kangaroo.py
+++ kangaroo.py
@@ -4,8 +4,17 @@ def kangaroo(kangaroo=[[],[]]):
         jump=int(input("enter the jump distance of kangaroo: "))
         kangaroo[i].append(location)
         kangaroo[i].append(jump)
-    if sum(kangaroo[0])==sum(kangaroo[1]):
-        print("yes,both kangaroos reach the same position at the same time")
-    else:
+    if ((kangaroo[0][0]>kangaroo[1][0])and (kangaroo[0][1]>kangaroo[1][1]))or((kangaroo[0][0]<kangaroo[1][0]) and
+    kangaroo[0][1]<kangaroo[1][1]):
         print("No,both kangaroos don't reach the same position at the same time")
+    else:
+        for i in range(0,10000):
+            kangaroo[0][0]=kangaroo[0][0]+kangaroo[0][1]
+            kangaroo[1][0]=kangaroo[1][0]+kangaroo[1][1]
+            if sum(kangaroo[0])==sum(kangaroo[1]):
+                print("Yes,both kangaroos reach the same position at the same time")
+                break
+        else:
+            print("No,both kangaroos don't reach the same position at the same time")
 kangaroo()
+
